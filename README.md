4be897a5754e381fbc8085ba8d9cd6d085dee340
rusty-electron <pritom.gogoi101@gmail.com>
2021-09-21 02:19:17 +0530

    refactor code and add new features

--- README.md
+++ README.md
@@ -3,8 +3,26 @@
 A script to set the daily bing image as the wallpaper. It can also set from a random set of previously fetched wallpapers.
 
 ```sh
-python3 daily.py -h
-Help menu
-default: Set the current day bing wallpaper
--r: Set a random image from the past bing wallpapers
+usage: daily.py [-h] [--random] [-p] [-d ROOT_DIR] [--viewer VIEWER]
+                [--setter {feh,nitrogen,hsetroot}]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --random              choose a random image from root_dir
+  -p, --preview         only preview image, don't set/save
+  -d ROOT_DIR, --root-dir ROOT_DIR
+                        path to root directory
+  --viewer VIEWER       specify preview tool
+  --setter {feh,nitrogen,hsetroot}
+                        specify tool to use when setting wallpaper
 ```
+
+> You may change the default directory and default application by making small edits to the `daily.py` file.
+## Dependencies
+
+* python libraries
+    * `requests`
+    * `urllib`
+    * `argparse`
+* others
+    * `hsetroot` | `nitrogen` | `feh` - application that can set wallpaper
--- daily.py
+++ daily.py
@@ -1,63 +1,84 @@
-import requests, urllib.request, os, random, time, sys
+import requests, urllib.request, os, random, sys
 from datetime import datetime
+import argparse
 
-root_dir = "~/Pictures/BingWallpapers/"
+root_dir = "~/Pictures/bing"
+wallpaper_setters = ["feh", "nitrogen", "hsetroot"]
 
-def dailyImage (): 
+parser = argparse.ArgumentParser()
 
-    response = requests.get("https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US").json()
+parser.add_argument("--random", help="choose a random image from root_dir", action="store_true")
+parser.add_argument("-p", "--preview", help="only preview image, don't set/save", action="store_true")
+parser.add_argument("-d", "--root-dir", help="path to root directory", default=root_dir)
+parser.add_argument("--viewer", help="specify preview tool", default="xdg-open")
+parser.add_argument("--setter", help="specify tool to use when setting wallpaper", choices=wallpaper_setters, default="hsetroot")
 
-    url = "https://bing.com" + response['images'][0]['url']
+args = vars(parser.parse_args())
 
-    urllib.request.urlretrieve(url, "daily.jpg")
-    
-    cmd = "hsetroot -cover daily.jpg"
-    os.system(cmd) 
-
-    fileName = datetime.now().strftime("%d%m%Y")
-
-    cmd = "cp daily.jpg " + (root_dir + fileName + ".jpg")
+def set_wallpaper(img_name, setter_app):
+    if setter_app == "hsetroot":
+        cmd = f"hsetroot -cover {img_name}"
+    elif setter_app == "feh":
+        cmd = f"feh --bg-fill {img_name}" # note that this sets it on all monitors
+    elif setter_app == "nitrogen":
+        cmd = f"nitrogen {img_name}"
     os.system(cmd)
 
-def randomImage (): 
+def download_daily_image():
+    response = requests.get("https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US").json()
+    url = "https://bing.com" + response['images'][0]['url']
+    filename, _ = urllib.request.urlretrieve(url, "/tmp/daily.jpg")
+    return filename
 
-    command = "ls " + root_dir
+def save_wallpaper():
+    fileName = datetime.now().strftime("bing-%d-%m-%Y")
+    savepath = os.path.join(root_dir, fileName) + '.jpg'
+    cmd = f"cp /tmp/daily.jpg {savepath}"
+    os.system(cmd)
+    return savepath
 
-    ls = os.popen(command).read().split('\n')
+def preview(img_path, preview_tool):
+    # this should work for most programs, i think
+    cmd = f"{preview_tool} {img_path}"
+    os.system(cmd)
 
-    if (len(ls) == 0 or set(ls) == set([''])): 
-        print ("No images found")
-        return
+def get_random_image(filepath):
+    filelist = []
+    for root, _, files in os.walk(filepath):
+        for filename in files:
+            if filename.split(".")[-1] in ["jpeg", "png", "jpg"]: # add more extensions if needed
+                filelist.append(os.path.join(root, filename))
 
-    random.seed(time.perf_counter())
-    picture = root_dir + (random.choice(ls))
+    if len(filelist) == 0:
+        print("[Error] directory is empty!")
+        sys.exit(1)
+    return random.choice(filelist)
 
-    print ("Setting " + picture)
+def make_dir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+        return 0
+    return 1
 
-    command = "hsetroot -cover " + picture
+def fullpath(shortpath):
+    return os.path.expanduser(shortpath)
 
-    os.system(command)
+if __name__ == "__main__":
+    # making sure that the root-dir exists at startup
+    root_dir = fullpath(root_dir)
+    make_dir(root_dir)
 
-command = "ls ~/Pictures"
+    if args["random"]:
+        fname = get_random_image(root_dir)
+    else:
+        # just download
+        fname = download_daily_image()
 
-ls = os.popen(command).read().split('\n')
-
-if ("BingWallpapers" not in ls):
-    os.system("mkdir ~/Pictures/BingWallpapers/")
-
-argumentList = sys.argv[1:]
-
-if len(argumentList) > 0:
-
-    argumentList = argumentList[0]
-
-    if argumentList in ["-h", "--help"]:
-        print ("Help menu\n\
-default: Set today's bing wallpaper\n\
--r: Set a random image from the past bing wallpapers")
-
-    elif argumentList in ["-r", "--random"]:
-        randomImage()
-
-else:
-    dailyImage()
+    if args["preview"]:
+        preview(fname, args["viewer"])
+    else:
+        if args["random"]:
+            set_wallpaper(fname, args["setter"])
+        else:
+            savepath = save_wallpaper()
+            set_wallpaper(savepath, args["setter"])
